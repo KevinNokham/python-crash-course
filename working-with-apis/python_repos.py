8f78a45f29598d71d1440c47bfbc83bd5bb89a02
KevinNokham <kevinnokham@gmail.com>
2022-03-12 22:16:10 -0500

    Move location for file generation

--- working-with-apis/python_repos.py
+++ working-with-apis/python_repos.py
@@ -51,4 +51,4 @@ my_layout = {
 }
 
 fig = {'data': data, 'layout': my_layout}
-offline.plot(fig, filename = 'python_repos.html')
+offline.plot(fig, filename = 'working-with-apis/python_repos.html')
