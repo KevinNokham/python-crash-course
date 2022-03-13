2003dc55f7cfd4309f81397fedd660b5a327f00e
KevinNokham <kevinnokham@gmail.com>
2022-03-12 22:15:18 -0500

    Add basic API calling and visualization

---
+++ working-with-apis/python_repos.py
@@ -0,0 +1,54 @@
+import requests
+
+from plotly.graph_objs import bar
+from plotly import offline
+
+# Make an API call and store the response
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers = headers)
+print(f"Status code: {r.status_code}")
+
+# Process results
+response_dict = r.json()
+repo_names, stars = [], []
+print(f"Total repositories: {response_dict['total_count']}")
+
+# Explore information abotu the repositories.
+repo_dicts = response_dict["items"]
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# Examine each repository.
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Make visualization
+data = [{
+    'type': 'bar',
+    'x': repo_names,
+    'y': stars,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+    },
+    'opacity': 0.6,
+}]
+
+my_layout = {
+    'title': 'Most-Starred Python Projects on GitHub',
+    'titlefont': {'size': 28},
+    'xaxis' : {
+        'title': 'Repository',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    },
+    'yaxis': {
+        'title': 'Stars',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    },
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename = 'python_repos.html')
